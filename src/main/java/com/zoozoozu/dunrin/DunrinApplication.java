bff689fdd3328a280b3853c32af505fdb8ed760c
hj-desktop <12hjpoint@gmail.com>
2023-01-08 03:57:09 +0900

    init

---
+++ src/main/java/com/zoozoozu/dunrin/DunrinApplication.java
@@ -0,0 +1,13 @@
+package com.zoozoozu.dunrin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunrinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunrinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoozoozu/dunrin/DunrinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoozoozu.dunrin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunrinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
