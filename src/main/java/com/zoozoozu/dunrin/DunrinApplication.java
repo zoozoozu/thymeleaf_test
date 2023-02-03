562da81166f300a9a46d7c7f0552aecf949bbdde
zoozoozu <hjlee@macbook.co.kr>
2023-02-03 17:23:58 +0900

    bootstrap이용 Menu nav 구성

--- src/main/java/com/zoozoozu/dunrin/DunrinApplication.java
+++ src/main/java/com/zoozoozu/dunrin/DunrinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DunrinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DunrinApplication.class, args);
 	}
